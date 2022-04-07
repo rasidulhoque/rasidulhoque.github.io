9a38f5cd02cac38d1d20948086823c28115705f0
RASIDUL HOQUE <77561833+rasidulhoque@users.noreply.github.com>
2022-04-07 23:24:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var a=prompt("Enter your puppy's name");
+  	if(a== "messi")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
